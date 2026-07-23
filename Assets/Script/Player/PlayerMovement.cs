2bf6a4bcf19baedd7c09e507a0612ae343c3d760
Yamaguchi Tenga <ca01264034@st.kawahara.ac.jp>
2026-07-23 18:14:18 +0900

    Recovery
    修正

--- Assets/Script/Player/PlayerMovement.cs
+++ Assets/Script/Player/PlayerMovement.cs
@@ -40,8 +40,7 @@ public class PlayerMovement : MonoBehaviour
         Vector2 moveInput = input.Player.Move.ReadValue<Vector2>();
         // 入力があるかどうか
         bool isMoving = moveInput.sqrMagnitude > 0.01f;
-        //アニメーション
-        animator.SetBool("Run", isMoving);
+ 
         // Vector2をVector3へ変換する
         Vector3 moveDirection = new Vector3(
             moveInput.x,
