9bc00b1023e743f1373595846e3c1e2b595860d9
Hyodo Takuma <ca012640228@st.kawahara.ac.jp>
2026-07-27 10:24:21 +0900

    ChangeScene

--- Assets/Script/SceneChange.cs
+++ Assets/Script/SceneChange.cs
@@ -1,25 +1,41 @@
+using System.Runtime.CompilerServices;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneChange : MonoBehaviour
 {
+    private Pouse pouse;
+   
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        pouse = GetComponent<Pouse>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        SceneLoop();
+
+        //Mainシーンの時だけポーズ処理
+        if (SceneManager.GetActiveScene().name == "Main")
+        {
+            pouse.Pause();
+        }
+    }
+
+    private void SceneLoop()
+    {
         if (Keyboard.current.enterKey.wasPressedThisFrame)
         {
             if (SceneManager.GetActiveScene().name == "Title")
             {
-                SceneManager.LoadScene("Game");
+                SceneManager.LoadScene("Main");
             }
-            else if (SceneManager.GetActiveScene().name == "Game")
+            else if (SceneManager.GetActiveScene().name == "Main")
             {
                 SceneManager.LoadScene("Result");
             }
